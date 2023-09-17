aafba17c9a7829b3f1e836dcafddaef7baa1be4a
mong3125 <mong3125@naver.com>
2023-09-17 12:56:20 +0900

    initial commit

---
+++ src/test/java/apptive/london/bridge/BridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package apptive.london.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
