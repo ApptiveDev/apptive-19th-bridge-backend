7b215485ce2711aba1942f5d36bafbb8f0ad2531
mong3125 <mong3125@naver.com>
2023-09-21 12:46:51 +0900

    refactor: exception handler 분리

--- src/main/java/apptive/london/bridge/global/auth/local/exception/AuthenticationExceptionHandler.java
+++ src/main/java/apptive/london/bridge/global/auth/local/exception/AuthenticationExceptionHandler.java
@@ -1,13 +1,12 @@
-package apptive.london.bridge.global.error.exception;
+package apptive.london.bridge.global.auth.local.exception;
 
-import apptive.london.bridge.global.auth.local.exception.EmailAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class AuthenticationExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<String> handlerEmailAlreadyExistsException(EmailAlreadyExistsException e) {
