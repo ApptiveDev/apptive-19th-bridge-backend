20440538bd2cc9138d34936a82f611f74f8f6703
mong3125 <mong3125@naver.com>
2023-09-21 15:50:27 +0900

    refactor: request json field명 수정

--- src/main/java/apptive/london/bridge/domain/user/dto/ModifyCreatorRequest.java
+++ src/main/java/apptive/london/bridge/domain/user/dto/ModifyCreatorRequest.java
@@ -1,5 +1,6 @@
 package apptive.london.bridge.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -9,10 +10,14 @@ import java.util.List;
 public class ModifyCreatorRequest {
     private String nickname;
     private String birthday;
+
+    @JsonProperty("creator_name")
     private String creatorName;
     private String gender;
 
+    @JsonProperty("channel_links")
     private List<String> channelLinks = new ArrayList<>();
 
+    @JsonProperty("business_email")
     private String businessEmail;
 }
